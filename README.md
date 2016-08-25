55337a7a29d2805818b7c6eb20e2188ab7023181
jsonlitz <jasondotlee@gmail.com>
2016-08-25 16:14:04 -0700

    add users routes

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # README
-testetst
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "/" => "climbs#index"
+  get  "/users" => "users#index", as: "users"
+  get "/users/new" => "users#new", as: "new_user"
+  post "/users" => "users#create"
+  get "/users/:id" => "users#show", as: "user"
+  get "/users/:id" => "users#edit"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
+
+  # get "/climbs", => "climbs#index", as: "climbs"
+  # get "/climbs/new", => "climbs#new", as: "new_climb"
+  # post "/climbs", => "climbs#create"
+  # get "/climbs/:id", => "climbs#show", as: "climb"
+
+  get "/login"
 end
