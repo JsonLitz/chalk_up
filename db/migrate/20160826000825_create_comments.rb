cc5d746f3a64dae3506c7c087aa3cedc35fc2a8f
Ryan Brenner <ryanbrenner@Ryans-MacBook-Air-1.local>
2016-08-26 08:39:07 -0700

    changed order of migration files by swapping prefixes to avoid further errors migrating comments without the climbs table yet created but references in comments

---
+++ db/migrate/20160826000825_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.string :title
+      t.text :body
+      t.references :user, foreign_key: true
+      t.references :climb, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
+
