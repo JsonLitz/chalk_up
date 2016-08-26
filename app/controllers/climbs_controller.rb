0ea6c160306e945aefa9a847b09a976ed8ae1018
mehgellan <baumegg@gmail.com>
2016-08-26 16:23:10 -0700

    add google map to climbs index page--not sf

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -2,6 +2,11 @@ class ClimbsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
+    @climbs = Climb.all
+    @hash = Gmaps4rails.build_markers(@users) do |climbs, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+    end
   end
 
   def new
