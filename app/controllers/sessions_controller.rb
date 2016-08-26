a78ddbf3fa2ae34d700245b99c194048412015fd
Ted Anyansi <tanyansi@gmail.com>
2016-08-25 18:47:05 -0700

    defined sessions controller methods and sessions helper methods and a login view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,32 @@
 class SessionsController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
-
+    @user = User.confirm(user_params)
+    if @user
+      flash[:success] = "Login successful! Welcome #{user.username}!"
+      login(@user)
+      redirect_to @user
+    else
+      flash[:error] = "Incorrect email or password. Please try again"
+      redirect_to login_path
+    end
   end
 
   def destroy
-
+    flash[:success] = "Logout successful. See you soon!"
+    logout
+    redirect_to root_path
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 
 
 end
