8458b056236eab652b4dcb8aa331a4f0b168762d
jsonlitz <jasondotlee@gmail.com>
2016-08-27 13:57:00 -0700

    merge changes

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -2,6 +2,11 @@ class ClimbsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
+    @climbs = Climb.all
+    @hash = Gmaps4rails.build_markers(@climbs) do |climb, marker|
+      marker.lat climb.latitude
+      marker.lng climb.longitude
+    end
   end
 
   def new
