520f193e678b76719b77f0f340001ba799d79845
Ted Anyansi <tanyansi@gmail.com>
2016-08-27 14:18:39 -0700

    setting up climbs controller

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -2,15 +2,24 @@ class ClimbsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
+    @climbs = Climb.all
+
   end
 
   def new
+    @climb = Climb.new(climb_params)
+    @climb.save
+
   end
 
   def create
   end
 
   def show
+  end
+
+  def climb_params
+      params.require(:climb).permit(:name,:image,:longitude, :latitude, :geolocation,:rating, :gear)     
   end
 
 end
