e1b5ba91cdc5126337ea17ffd4b1bfa9458404ab
jsonlitz <jasondotlee@gmail.com>
2016-08-28 14:02:59 -0700

    add password and submission validation

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -1,2 +1,10 @@
 class Climb < ApplicationRecord
+  VALID_IMAGE_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :image,
+    presence:   true,
+    format:     { with: VALID_IMAGE_REGEX },
+    uniqueness: true,
+    length:     { maximum: 255 }
+
 end
