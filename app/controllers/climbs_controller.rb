1d0a9978b2387aa26b16c41120d4b88ebca8f58f
Ted Anyansi <tanyansi@gmail.com>
2016-08-27 17:38:02 -0700

    update and delete for climbs work now but not yet a button to actually delete nor is there a show page for the climb location

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -1,5 +1,6 @@
 class ClimbsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
+  before_action :set_climb, only: [:show, :edit, :update, :destroy]
 
   def index
     @climbs = Climb.all
@@ -21,8 +22,34 @@ class ClimbsController < ApplicationController
   def show
   end
 
-  def climb_params
-      params.require(:climb).permit(:name,:image,:longitude, :latitude, :geolocation,:rating, :gear, :style, :gym?)
+  def edit
+
+    @climb = Climb.find(params[:id])
+
   end
+
+  def update
+
+    @climb.update(climb_params)
+    redirect_to root_path
+
+  end
+
+  def destroy
+
+    @climb.destroy
+    redirect_to root_path
+
+  end
+
+  private
+
+    def set_climb
+        @climb = Climb.find(params[:id])
+    end
+
+    def climb_params
+        params.require(:climb).permit(:name,:image,:longitude, :latitude, :geolocation,:rating, :gear, :style, :gym?)
+    end
 
 end
