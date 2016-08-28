5e8a1e862ffe895ab51678cfe80183c36e42b291
jsonlitz <jasondotlee@gmail.com>
2016-08-28 12:29:18 -0700

    add formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :determine_navlinks
-
-    protect_from_forgery with: :exception
-
-    include SessionsHelper
+  protect_from_forgery with: :exception
+  include SessionsHelper
   include ApplicationHelper
-
-  end
+end
--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -1,29 +1,44 @@
 class ClimbsController < ApplicationController
-   skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index]
+  before_action :set_climb, only: [:show, :edit, :update, :destroy]
 
-   def index
--    @climbs = Climb.all
--    @hash = Gmaps4rails.build_markers(@climbs) do |climb, marker|
--      marker.lat climb.latitude
--      marker.lng climb.longitude
--    end
-+
-   end
+  def index
+  end
 
-   def new
-     @climb = Climb.new
-   end
+  def new
+    @climb = Climb.new
+  end
 
-   def create
--    @climb = Climb.new(climb_params)
--    @climb.save
-+    @climb = Climb.create(climb_params)
-+
-   end
+  def create
+    @climb = Climb.create(climb_params)
+  end
 
-   def show
-   end
-+private
+  def show
+  end
 
-   def climb_params
-       params.require(:climb).permit(:name,:image,:longitude, :latitude, :geolocation,:rating, :gear, :style, :gym?)
+  private
+
+  def edit
+    @climb = Climb.find(params[:id])
+  end
+
+  def update
+    @climb.update(climb_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @climb.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_climb
+    @climb = Climb.find(params[:id])
+  end
+
+  def climb_params
+    params.require(:climb).permit(:name,:image,:longitude, :latitude, :geolocation,:rating, :gear, :style, :gym?)
+  end
+end
