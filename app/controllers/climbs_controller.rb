8bc5562e7952b13475cb864dcbf5fd30edd1d1b8
Ryan Brenner <ryanbrenner@Ryans-MacBook-Air-1.local>
2016-08-28 09:55:58 -0700

    added logic to the application helper which will render different links site wide depending on login status

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -2,11 +2,8 @@ class ClimbsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @climbs = Climb.all
-    @hash = Gmaps4rails.build_markers(@climbs) do |climb, marker|
-      marker.lat climb.latitude
-      marker.lng climb.longitude
-    end
+     
+
   end
 
   def new
@@ -14,12 +11,13 @@ class ClimbsController < ApplicationController
   end
 
   def create
-    @climb = Climb.new(climb_params)
-    @climb.save
+    @climb = Climb.create(climb_params)
+  
   end
 
   def show
   end
+private
 
   def climb_params
       params.require(:climb).permit(:name,:image,:longitude, :latitude, :geolocation,:rating, :gear, :style, :gym?)
