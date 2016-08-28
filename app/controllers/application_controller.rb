b071a62cce08e2bd1eaf4e0d10d41e99a51eb32d
jsonlitz <jasondotlee@gmail.com>
2016-08-28 12:20:50 -0700

    add logic for header partial and template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+    before_action :authenticate_user!
+  before_action :determine_navlinks
 
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
 
-  include SessionsHelper
+    include SessionsHelper
+  include ApplicationHelper
 
-end
+  end
