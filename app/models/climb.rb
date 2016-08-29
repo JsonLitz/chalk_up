b1d3d7b9e45ace07da185676d2b3ac08724d559e
mehgellan <baumegg@gmail.com>
2016-08-28 20:02:26 -0700

    add validations for presence in climb model and create form partial for new climb

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -1,3 +1,4 @@
 class Climb < ApplicationRecord
-
+  validates :name, :image, :rating, :gear, :style, presence: { message: "%{value} must be given please"}
+  validates :geolocation, :gym?, inclusion: { in: [true, false] }
 end
