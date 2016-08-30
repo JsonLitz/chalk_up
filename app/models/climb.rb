d4f7f9a8280f9634ff806fe87da648c89d9d5ed1
mehgellan <baumegg@gmail.com>
2016-08-29 18:51:51 -0700

    style for climb show partial and a few extra edits

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -2,6 +2,4 @@ class Climb < ApplicationRecord
 
   has_many :comments
   validates :name, :image, :rating, :gear, :style, presence: { message: "%{value} must be given please"}
-  validates :geolocation, :gym?, inclusion: { in: [true, false] }
-
 end
