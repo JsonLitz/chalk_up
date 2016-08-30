904c2f17a3bb1e230520003a258a394ba3d7cbb2
Ted Anyansi <tanyansi@gmail.com>
2016-08-29 19:09:26 -0700

    a user's climb locations they've commented on is showed on their show page

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -1,6 +1,7 @@
 class Climb < ApplicationRecord
 
   has_many :comments
+  has_many :users, :through => :comments
   validates :name, :image, :rating, :gear, :style, presence: { message: "%{value} must be given please"}
   # validates :geolocation, :gym?, inclusion: { in: [true, false] }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :comments
+  has_many :climbs, :through => :comments
+
   validates :username, presence: true, length: { maximum: 15 }
   validates :password_digest, presence: true
 
