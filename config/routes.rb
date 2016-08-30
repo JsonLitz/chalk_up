218a6a11d04ba3fa1c81bef1159532ff6f6e8ab9
Ted Anyansi <tanyansi@gmail.com>
2016-08-29 19:44:37 -0700

    changed to show boolean values to yes or no

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 
   get "/comments" => "comments#index", as: "comments"
   get "/climbs/:id/comments/new" => "comments#new", as: "new_comment"
-  post "/climbs/:id/comments" => "comments#create"
+  post "/climbs/:id/comments/new" => "comments#create"
   get "/comments/:id" => "comments#show", as: "comment"
   get "/comments/:id/edit" => "comments#edit", as: "edit_comment"
   patch "/comments/:id" => "comments#update"
