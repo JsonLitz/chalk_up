b4d3638bdaaa45e33e36cdf94768c24106048156
Ted Anyansi <tanyansi@gmail.com>
2016-08-30 18:29:24 -0700

    authorization for editing and deleting comments exclusively to the user who made that comment

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,6 @@ Rails.application.routes.draw do
   get "/comments/:id" => "comments#show", as: "comment"
   get "/comments/:id/edit" => "comments#edit", as: "edit_comment"
   patch "/comments/:id" => "comments#update"
-  delete "/comments/:id" => "comments#destroy"
+  delete "/comments/:id" => "comments#destroy", as: "delete_comment"
 
 end
