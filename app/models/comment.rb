7b134da6be0f0de5be8cf1c097cc7ec4f80c9524
Ted Anyansi <tanyansi@gmail.com>
2016-08-30 20:39:24 -0700

    added validation for comments and logic to handle failed update/create actions for comments. also added confirm pop up for delete comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :climb
+
+  validates :title, length: {in: 2..30}, presence: true
+  validates :body, length: {in: 2..300}, presence: true
+
 end
