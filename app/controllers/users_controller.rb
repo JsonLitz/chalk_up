e559e79355e60d544af4c5f97510447d709c9104
mehgellan <baumegg@gmail.com>
2016-08-31 17:03:52 -0700

    add checkin info to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,12 @@ class UsersController < ApplicationController
  end
 
  def show
+  @climb_name = []
+  @user.checkins.each.with_index do |checkin|
+    justin_is_my_copilot = checkin.climb_id
+    climb_name = climb_name(justin_is_my_copilot)
+    @climb_name << climb_name
+  end
  end
 
  def edit
@@ -48,6 +54,10 @@ class UsersController < ApplicationController
  end
 
  private
+
+ def climb_name(id)
+   Climb.find_by_id(id).name
+ end
 
  def res
   request.path.split('/')[1][0..-2]
